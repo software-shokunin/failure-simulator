0b69513b2c07680fbbc33367b0590d1ac179d890
ymartin <ynmartin@gmail.com>
2016-07-06 12:08:08 +1000

    Changed spaces to tabs

--- customer/src/app.js
+++ customer/src/app.js
@@ -5,11 +5,11 @@ const PORT    = process.env.CUSTOMER_PORT || 8888;
 let app       = express();
 
 app.get('/', (req, res) => {
-    res.send('Customer is king');
+	res.send('Customer is king');
 });
 
 app.listen(PORT, () => {
-  const colors  = require('colors/safe');
-  const message = `Customer API service running on port: ${PORT}`;
-  console.log(colors.green.underline(message));
+	const colors  = require('colors/safe');
+	const message = `Customer API service running on port: ${PORT}`;
+	console.log(colors.green.underline(message));
 });
