3ebf33d7938b30686440ef3e74198eb079384a7c
Ray Tung <rtung@thoughtworks.com>
2016-07-06 14:12:46 +1000

    Now uses jasmine 2.0 instead of jasmine-node for testing

--- customer/spec/helloWorldSpec.js
+++ customer/spec/helloWorldSpec.js
@@ -1,10 +1,10 @@
-let request = require("request");
-let base_url = "http://localhost:3000/"
+let request    = require('request');
+const BASE_URL = "http://localhost:3000/"
 
-describe("Hello World Server", function() {
-	describe("GET /", function() {
-		it("returns status code 200", function() {
-			request.get(base_url, function(error, response, body) {
+describe('Hello World Server', () => {
+	describe('GET /', () => {
+		it('returns status code 200', () => {
+			request.get(BASE_URL, (error, response, body) => {
 				expect(response.statusCode).toBe(200);
 				done();
 			});
