224a066669bebf012e9f9d2a2672ca6f3c05c8bd
Ray Tung <rtung@thoughtworks.com>
2016-07-06 12:01:17 +1000

    Added message to see if server is running

--- customer/src/app.js
+++ customer/src/app.js
@@ -1,10 +1,13 @@
 'use strict';
-const express = require('express');
 
-let app = express();
+const express = require('express');
+const PORT    = process.env.CUSTOMER_PORT || 8888;
+let app       = express();
 
 app.get('/', (req, res) => {
-    res.send("qwe");
+    res.send('Customer is king');
 });
 
-app.listen(8888);
+app.listen(PORT, () => {
+  console.log(`Customer API service running on port: ${PORT}`);
+});
