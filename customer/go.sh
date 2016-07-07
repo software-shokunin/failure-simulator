65ed5ab86edc0115c680d1e6c9baac409896a37c
ymartin <ynmartin@gmail.com>
2016-07-07 16:53:19 +1000

    Modified script to use docker compose

--- customer/go.sh
+++ customer/go.sh
@@ -1,30 +1,8 @@
-image=failure-simulator-node
-container=failure-simulator-customer
-port=8888
-
-stop_container() {
-  docker ps -a | grep $container && docker stop $container && docker rm -f $container
-}
-
-delete_untagged_images() {
-  docker images --no-trunc -aqf "dangling=true" | xargs docker rmi
-}
-
-if [[ $1 = 'start' ]]
+if [[ $1 = 'test' ]]
 then
-  stop_container
-  docker run -p $port:$port -it -v "$PWD":/usr/src/app --name $container $image bash -c 'npm install; npm start'
-  exit
-elif [[ $1 = 'test' ]]
-then
-  stop_container
-  docker run -p $port:$port -it -v "$PWD":/usr/src/app --name $container $image bash -c 'npm install; npm test'
-elif [[ $1 = 'build' ]]
-then
-  stop_container
-  docker build -t $image -f Dockerfile .
-  delete_untagged_images
-  exit
+  docker-compose up -d
+  docker-compose exec app npm test
+  docker-compose down
 else
   echo "Valid commands: start|test|build"
 fi
