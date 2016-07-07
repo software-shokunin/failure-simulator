c63c674ab755fc25b1d63b93a4a6ab6f496829e5
ymartin <ynmartin@gmail.com>
2016-07-07 13:54:52 +1000

    Cleaned up error status

--- customer/src/routes/customer.js
+++ customer/src/routes/customer.js
@@ -2,22 +2,18 @@
 
 const router    = require('express').Router();
 const customers = require('../../customers.json');
-const NOT_FOUND = {
-		status: 404,
-		developerMessage: 'customer not found'
-};
 
 router.get('/', (req, res, next) => {
-		res.send('Customer is king');
+	res.send('Customer is king');
 });
 
 router.get('/:id', (req, res, next) => {
-		let customer = customers[req.params.id];
-		if(customer) {
-				res.status(302).send(customer);
-				return;
-		}
-		res.status(404).send(NOT_FOUND);
+	let customer = customers[req.params.id];
+	if(customer) {
+		res.status(302).send(customer);
+	} else {
+		res.sendStatus(404);
+	}
 });
 
 module.exports = router;
--- customer/test/customer.spec.js
+++ customer/test/customer.spec.js
@@ -8,42 +8,38 @@ describe("customers", () => {
 	describe('get customer by id', () => {
 		it('returns customer 1 details', (done) => {
 			request(app)
-				.get('/1')
-				.expect(302)
-				.expect({
-					"id"   : "1",
-					"name" : "Customer 1",
-					"paymentDetails": {
-								"number" : "4000 0000 0000 0002",
-								"ccv"    : "123",
-								"expiry" : "11/20"
-					}
-				}, done)
+			.get('/1')
+			.expect(302)
+			.expect({
+				"id"   : "1",
+				"name" : "Customer 1",
+				"paymentDetails": {
+					"number" : "4000 0000 0000 0002",
+					"ccv"    : "123",
+					"expiry" : "11/20"
+				}
+			}, done)
 		});
 
 		it('returns customer 2 details', (done) => {
 			request(app)
-				.get('/2')
-				.expect(302)
-				.expect({
-					"id"  : "2",
-					"name": "Customer 2",
-					"paymentDetails": {
-								"number" : "5100 0000 0000 0008",
-								"ccv"    : "567",
-								"expiry" : "11/20"
-					}
-				}, done)
+			.get('/2')
+			.expect(302)
+			.expect({
+				"id"  : "2",
+				"name": "Customer 2",
+				"paymentDetails": {
+					"number" : "5100 0000 0000 0008",
+					"ccv"    : "567",
+					"expiry" : "11/20"
+				}
+			}, done)
 		});
 
-    it('returns not found if customer cannot be found with given id', (done) => {
-        request(app)
-						.get('/abc')
-						.expect(404)
-						.expect({
-								status: 404,
-								developerMessage: 'customer not found'
-						}, done);
-    });
+		it('returns 401 when customer not found', (done) => {
+			request(app)
+			.get('/abc')
+			.expect(404, done);
+		});
 	});
 });
